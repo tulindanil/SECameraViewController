6b6c6da534de9512be0bfccccfeb753dec9270d9
tulindanil <tulindanil@gmail.com>
2016-01-26 23:15:14 +0300

    Initial commit

---
+++ Pod/Classes/SECameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  SECameraViewController.h
+//  Pods
+//
+//  Created by Danil Tulin on 1/26/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SECameraViewController : UIViewController
+
+@end
---
+++ Pod/Classes/SECameraViewController.m
@@ -0,0 +1,13 @@
+//
+//  SECameraViewController.m
+//  Pods
+//
+//  Created by Danil Tulin on 1/26/16.
+//
+//
+
+#import "SECameraViewController.h"
+
+@implementation SECameraViewController
+
+@end
