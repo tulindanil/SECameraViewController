f7b4e47cb0b5d8e89504ec1e1c60b6cf15675a1d
tulindanil <tulindanil@gmail.com>
2016-02-16 13:36:31 +0300

    add colours

--- Pod/Classes/Constants.h
+++ Pod/Classes/Constants.h
@@ -6,8 +6,11 @@
 //
 //
 
+@import Foundation;
+
 #ifndef Constants_h
 #define Constants_h
 
-
+const static NSString *defaultPrimaryColor = @"1A1A1C";
+const static NSString *darkPrimaryColor = @"121315";
 #endif /* Constants_h */
