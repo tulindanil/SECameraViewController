46bd1ed85acbe1ee31aa12aedb9d96f87f30c96a
tulindanil <tulindanil@gmail.com>
2016-02-16 13:33:57 +0300

    move files in project list

---
+++ Pod/Classes/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Pods
+//
+//  Created by Danil Tulin on 1/30/16.
+//
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
---
+++ Pod/Classes/SEUIKit.h
@@ -0,0 +1,13 @@
+//
+//  SEUIKit.h
+//  Pods
+//
+//  Created by Danil Tulin on 1/30/16.
+//
+//
+
+#ifndef SEUIKit_h
+#define SEUIKit_h
+
+
+#endif /* SEUIKit_h */
