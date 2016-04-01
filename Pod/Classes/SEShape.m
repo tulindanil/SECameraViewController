76740a6457f894f64c9513a0716bf21ce17c663a
tulindanil <tulindanil@gmail.com>
2016-04-02 01:09:48 +0300

    drawInContext: is done

--- Pod/Classes/SEShape.m
+++ Pod/Classes/SEShape.m
@@ -37,7 +37,21 @@
 #pragma mark - drawing
 
 - (void)drawInContext:(CGContextRef)context {
+	CGContextSetLineWidth(context, 1.5f);
+	CGContextSetAlpha(context, 0.8f);
+	CGContextSetLineCap(context, kCGLineCapRound);
+	CGContextSetLineJoin(context, kCGLineJoinRound);
 	
+	CGContextSetLineWidth(context, 1.0f);
+	
+	CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
+	CGContextBeginPath(context);
+	
+	for (SEPoint *point in self.internal) {
+		CGContextAddLineToPoint(context, point.x, point.y);
+	}
+	
+	CGContextStrokePath(context);
 }
 
 #pragma mark - Internal
