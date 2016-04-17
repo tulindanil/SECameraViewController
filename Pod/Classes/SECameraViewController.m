e94124f7d569b109076423a4446394ff4ac537cc
tulindanil <tulindanil@gmail.com>
2016-04-17 17:45:58 +0300

    bug fix

--- Pod/Classes/SECameraViewController.m
+++ Pod/Classes/SECameraViewController.m
@@ -60,7 +60,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	self.view.backgroundColor = self.darkPrimaryColor;
+	self.view.backgroundColor = self.defaultPrimaryColor;
 	
 	[self.view addSubview:self.previewView];
     
