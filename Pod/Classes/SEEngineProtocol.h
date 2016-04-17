fb98e370c768f539a12126fcf0e4ae1907c4ef3f
tulindanil <tulindanil@gmail.com>
2016-04-18 00:17:24 +0300

    add area parameter

--- Pod/Classes/SEEngineProtocol.h
+++ Pod/Classes/SEEngineProtocol.h
@@ -9,13 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#import "SEShape.h"
+
 @protocol EngineProtocol <NSObject>
 
 @required
 
 - (void)feedBGRAImageData:(NSData *)data
 					width:(NSUInteger)width
-				   height:(NSUInteger)height;
+				   height:(NSUInteger)height
+                     area:(SEShape *)area;
 
 @property (nonatomic) float progress;
 @property (nonatomic) BOOL isAbleToProcess;
